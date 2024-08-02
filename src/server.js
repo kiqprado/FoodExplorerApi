c584ca1f1dc68b25a3315896607659b13d5d0331
Kaique Prado <kiqprado@outlook.com>
2024-08-02 03:39:16 -0300

    feat: Create and config Database at api.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
+require('express-async-errors')
 const express = require('express')
 
-const routes =  require('./routes')
+const database = require('./database/sqlite')
+
+const AppError = require('./utils/AppError')
+
+const routes = require('./routes')
+database()
 
 const app = express()
+
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
 
 const PORT = 3333
 app.listen(PORT, () => {

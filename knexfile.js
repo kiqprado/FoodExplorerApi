710629a90b16cbcba2bc81747485f8da76839d03
Kaique Prado <kiqprado@outlook.com>
2024-08-06 14:20:38 -0300

    fix: att knexFile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   development: {
@@ -6,9 +6,18 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
+    },
     migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'database',
+        'knex',
+        'migrations'
+      )
     },
     useNullAsDefault: true
   }
-};
+}

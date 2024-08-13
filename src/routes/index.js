c423497a630c3d4adc03455fe44f64e2b6da21aa
Kaique Prado <kiqprado@outlook.com>
2024-08-13 11:25:20 -0300

    feat: Add Sessions Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 
 const usersRouter = require('./users.routes.js')
+const sessionsRouter = require('./sessions.routes.js')
 const dishesRouter = require('./dishes.routes.js')
 const categoriesRouter = require('./categories.routes.js')
 const favoritesRouter = require('./favorites.routes.js')
@@ -8,9 +9,9 @@ const favoritesRouter = require('./favorites.routes.js')
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/dishes', dishesRouter)
 routes.use('/categories', categoriesRouter)
 routes.use('/favorites', favoritesRouter)
 
 module.exports = routes
-3 

9a860f457b8397b520378dc34fa1cdb0a0dd525f
Kaique Prado <kiqprado@outlook.com>
2024-08-15 06:45:46 -0300

    feat: Using cors on server file.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 require('express-async-errors')
 const express = require('express')
 
+const cors = require('cors')
+
 const database = require('./database/sqlite')
 
 const AppError = require('./utils/AppError')
 
+const uploadConfig = require('./configs/upload')
+
 const routes = require('./routes')
+
 database()
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
+
+app.use('/files', express.static(uploadConfig.TMP_FOLDER))
 
 app.use(routes)
 

1d7da88f50e1f2eb8f08d2df0dd7f83597a2454f
Kaique Prado <kiqprado@outlook.com>
2024-08-20 15:31:35 -0300

    feat: Creating another controller on Dishes, to make a single photo Avatar upload on Dish

---
+++ src/controllers/DishesAvatarController.js
@@ -0,0 +1,31 @@
+const knex = require('../database/knex')
+
+const DiskStorage = require('../providers/DiskStorage')
+
+const AppError = require('../utils/AppError')
+
+class DishesAvatarController {
+  async create(req, res) {
+    const { id } = req.params
+
+    const { filename: avatar } = req.file
+
+    const diskStorage = new DiskStorage()
+
+    const dish = await knex('dishes').where({ id }).first()
+
+    if (!dish) {
+      throw new AppError('Não foi encontrar o Prato')
+    }
+
+    const filename = await diskStorage.saveFile(avatar)
+
+    await knex('dishes').where({ id }).update({ avatar: filename })
+
+    return res.json({ avatar: filename })
+  }
+
+  async update(req, res) {}
+}
+
+module.exports = DishesAvatarController
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,13 +1,24 @@
 const { Router } = require('express')
 
+const multer = require('multer')
+
+const uploadConfig = require('../configs/upload')
+
 const DishesController = require('../controllers/DishesController')
+const DishesAvatarController = require('../controllers/DishesAvatarController')
 
 const dishesRoutes = Router()
 
+const upload = multer(uploadConfig.MULTER)
+
 const dishesController = new DishesController()
+const dishesAvatarController = new DishesAvatarController()
 
 dishesRoutes.post('/', dishesController.create)
+dishesRoutes.patch('/avatar/:id', upload.single('avatar'), dishesAvatarController.create)
+dishesRoutes.get('/', dishesController.index)
 dishesRoutes.get('/:id', dishesController.show)
 dishesRoutes.delete('/:id', dishesController.delete)
 
 module.exports = dishesRoutes
+  

b5be05d23fae3b993bcc4c57aaf07ac5bfeefa2b
Kaique Prado <kiqprado@outlook.com>
2024-08-28 08:26:10 -0300

    feat: Using VerifyAuthorization middleware

--- src/middlewares/verifyUserAuthorization.js
+++ src/middlewares/verifyUserAuthorization.js
@@ -4,7 +4,7 @@ function verifyUserAuthorization(roleToVerify) {
   return (req, res, next) => {
     const { role } = req.user
 
-    if (role !== roleToVerify) {
+    if (!roleToVerify.includes(role)) {
       throw new AppError('Unauthorized', 401)
     }
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,6 +7,9 @@ const uploadConfig = require('../configs/upload')
 const DishesController = require('../controllers/DishesController')
 const DishesAvatarController = require('../controllers/DishesAvatarController')
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization')
+
 const dishesRoutes = Router()
 
 const upload = multer(uploadConfig.MULTER)
@@ -14,11 +17,21 @@ const upload = multer(uploadConfig.MULTER)
 const dishesController = new DishesController()
 const dishesAvatarController = new DishesAvatarController()
 
-dishesRoutes.post('/', dishesController.create)
-dishesRoutes.patch('/avatar/:id', upload.single('avatar'), dishesAvatarController.create)
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.post(
+  '/',
+  verifyUserAuthorization(['admin']),
+  dishesController.create
+)
+dishesRoutes.patch(
+  '/avatar/:id',
+  upload.single('avatar'),
+  verifyUserAuthorization(['admin']),
+  dishesAvatarController.create
+)
 dishesRoutes.get('/', dishesController.index)
 dishesRoutes.get('/:id', dishesController.show)
 dishesRoutes.delete('/:id', dishesController.delete)
 
 module.exports = dishesRoutes
-  
